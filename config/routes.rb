f836b490f7ca9e4ed7cda14302e67f550c1c75a4
yuuki <f-1-clover4.saku@i.softbank.jp>
2023-10-16 12:14:08 +0900

    feat:管理画面へのログイン機能/管理画面トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,12 @@ Rails.application.routes.draw do
   resources :bookmarks, only: %i[create destroy]
 
   resource :profile, only: %i[show edit update]
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+  end
 end
 
 end
